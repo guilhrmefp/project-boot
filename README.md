c1dbd39a20e34ffcb747267a8c316eca0ab13660
Guilherme Fernandes Pereira <gfernape@usersad.everis.int>
2017-07-25 16:48:16 -0300

    Boot 2

--- README.md
+++ README.md
@@ -6,13 +6,32 @@
 * Node JS (base para o Gulp) - https://nodejs.org/
 * Gulp (via linha de comando):
 ```
-npm install gulp -g // Caso seja a primeira vez instalando o gulp, usar esse comando primeiro
-npm install gulp    // Após, usar esse comando para cada projeto
+npm install gulp -g //Caso já tenho o gulp instalado globalmente, ignorar este comando
 ```
+
+## Preenchendo os pré-requisitos, instalar módulos (presentes no arquivo package.json) ##
+```
+npm install
+```
+
+##Tasks específicas##
+* **sass**        => Gera e minifica CSS com vendor prefixes e sourcemap
+* **uglify**      => Minifica JS com sourcemap
+* **handlebars**  => Gera o html das partials
+* **images**      => Move as imagens da pasta 'src/img' para 'dist/img'
+* **fonts**       => Move os arquivos de fonte da pasta 'src/img' para 'dist/img'
+* **clean:dist**  => Apaga o diretório 'dist'
+* **clean:cache** => Limpa o cache do Gulp
+* **browserSync** => live-reload para os browsers
+* **watch**       => Arquivos que devem ter modificações vigiadas
+
+##Tasks produção##
+* **build**       => Compila todo o projeto para produção
+* **default**     => Compila todo o projeto (task 'build') e roda as tasks 'browserSycnc' e 'watch' para trabalho
 
 ## Pastas ignoradas no versionamento: ##
 * .sass-cache/ **-->** *(cache criado pelo sass)*
 * node_modules/ **-->** *(packages usados no projeto depois de 'npm install')*
-* npm-debug.log **-->** *(log do npm)*
-* \*\*/_notes **-->** *(arquivos geralmente criados pelo dreamweaver)*
+* npm-debug.log **-->** *(log do node)*
+* \*\*/_notes **-->** *(arquivos geralmente criados pelo Dreamweaver)*
 * dist/ **-->** *(pasta final com arquivo compilados)*
--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,108 @@
 /*=========================
 ||  Packages do projeto  ||
 =========================*/
-
-var gulp        = require('gulp');                    //gulp
-var sass        = require('gulp-sass');               //sass
-var compass     = require('gulp-compass');            //vendor prefixer de css
-var cssnano     = require('gulp-cssnano');            //minificadorr css
-var useref      = require('gulp-useref');             //criador de "blocos" de script no html
-var uglify      = require('gulp-uglify');             //minificador de js
-var sourcemaps  = require('gulp-sourcemaps');         //gerador de mapa para debuggar arquivo minificado
-var gulpIf      = require('gulp-if');                 //condicional de plugin
-var handlebars  = require('gulp-compile-handlebars'); //gerador de templates
-var dir         = require('node-dir');                //retorna subdiretórios
-var rename      = require('gulp-rename');             //renomear arquivos
-var del         = require('del');                     //exclui arquivos do diretório
-var runSequence = require('run-sequence')             //define a sequência em que as terefas devem ser iniciadas
-var browserSync = require('browser-sync').create();   //live-reload
+var gulp         = require('gulp');                    //gulp
+var sass         = require('gulp-sass');               //sass
+var postcss      = require('gulp-postcss');            //postcss
+var autoprefixer = require('autoprefixer');            //vendor prefixer de css
+var cssnano      = require('cssnano');                 //minificador css
+var uglify       = require('gulp-uglify');             //minificador js
+var sourcemaps   = require('gulp-sourcemaps');         //gerador de mapa para debuggar arquivo minificado
+var handlebars   = require('gulp-compile-handlebars'); //gerador de templates
+var dir          = require('node-dir');                //retorna subdiretórios
+var rename       = require('gulp-rename');             //renomear arquivos
+var del          = require('del');                     //exclui arquivos do diretório
+var cache        = require('gulp-cache');              //limpar cache
+var runSequence  = require('run-sequence')             //define a sequência em que as terefas devem ser iniciadas
+var browserSync  = require('browser-sync').create();   //live-reload
 
 
 /*=========================
 ||         TASKS         ||
 =========================*/
+browsersComp = [ //https://github.com/twbs/bootstrap-sass#sass-autoprefixer
+  "Android 2.3",
+  "Android >= 4",
+  "Chrome >= 20",
+  "Firefox >= 24",
+  "Explorer >= 8",
+  "iOS >= 6",
+  "Opera >= 12",
+  "Safari >= 6"
+]
 
-//task exemplo com sytaxe completa
-gulp.task('task-name', function() {
-  return gulp.src('source-files') // Get source files with gulp.src
-    .pipe(aGulpPlugin()) // Sends it through a gulp plugin
-    .pipe(gulp.dest('destination')) // Outputs the file in the destination folder
-});
-
-//gerar arquivos css
+//gerar arquivos css e minificar
 gulp.task('sass', function() {
-  return gulp.src('app/scss/**/*.+(scss|sass)') // Gets all files ending with .scss in app/scss and children dirs
-         .pipe(sass()) //usando plugin do sass
-         .pipe(gulp.dest('app/css'))
+  return gulp.src('src/sass/**/*.+(scss|sass)') //Gets all files ending with .scss in src/sass and children dirs
+         .pipe(sourcemaps.init())
+         .pipe(sass().on('error', sass.logError)) //usando plugin do sass
+         .pipe(postcss([
+            autoprefixer({browsers: browsersComp}),
+            cssnano()
+         ]))
+         .pipe(sourcemaps.write('/'))//cria sourcemap na pasta de css
+         .pipe(gulp.dest('dist/css'))
          .pipe(browserSync.reload({ //metodo adicionado para live-reload do browser
             stream: true
          }));
 });
 
-//juntar arquivos js e minificar (js e css)
-gulp.task('useref', function() {
-  return gulp.src('app/*.html')
-         .pipe(useref())
-         // Minifies only if it's a JavaScript file
-         .pipe(gulpIf('*.js', uglify()))
-         // Minifies only if it's a CSS file
-         .pipe(gulpIf('*.css', cssnano()))
-         .pipe(gulp.dest('dist'));
+//minificar js
+gulp.task('uglify', function(){
+  return gulp.src('src/js/**/*.js')
+         //.pipe(sourcemaps.init())
+         //.pipe(uglify())
+         //.pipe(sourcemaps.write('/'))
+         .pipe(gulp.dest('dist/js'))
+         .pipe(browserSync.reload({ //metodo adicionado para live-reload do browser
+            stream: true
+         }));
 });
 
 //gerar html dos templates
 gulp.task('handlebars', function() {
-  dir.subdirs('app/templates/partials', function(err, subdirs) {
+  dir.subdirs('src/templates/partials', function(err, subdirs) { //lista os subdiretórios com as partials
+    subdirs.push('src/templates/partials/'); //adiciona no array o diretório pai
+
     if (err) throw err;
-    return gulp.src('app/templates/pages/*.hbs')
+    return gulp.src('src/templates/pages/*.hbs')
            .pipe(handlebars({}, {
               ignorePartials: true,
               batch: subdirs
             }))
            .pipe(rename({extname: '.html'}))
-           .pipe(gulp.dest('dist'));
+           .pipe(gulp.dest('dist'))
+           .pipe(browserSync.reload({ //metodo adicionado para live-reload do browser
+              stream: true
+           }));
   });
+});
+
+//copiar imagens
+gulp.task('images', function() {
+  return gulp.src('src/img/**/*.+(png|jpg|jpeg|gif|svg)')
+         .pipe(gulp.dest('dist/img'))
+         .pipe(browserSync.reload({ //metodo adicionado para live-reload do browser
+            stream: true
+         }));
 });
 
 //copiar fontes
 gulp.task('fonts', function() {
-  return gulp.src('app/fonts/**/*')
-         .pipe(gulp.dest('dist/fonts'));
+  return gulp.src('src/fonts/*.+(eot|ttf|woff|woff2|svg)')
+         .pipe(gulp.dest('dist/fonts'))
+         .pipe(browserSync.reload({ //metodo adicionado para live-reload do browser
+            stream: true
+         }));
 });
 
-//limpar diretório
+//apagar diretório final
 gulp.task('clean:dist', function() {
   return del.sync('dist');
 });
 
 //limpar cache do gulp
-gulp.task('cache:clear', function(callback) {
+gulp.task('clean:cache', function(callback) {
   return cache.clearAll(callback);
 });
 
@@ -84,32 +110,29 @@ gulp.task('cache:clear', function(callback) {
 gulp.task('browserSync', function(){
   browserSync.init({
     server: {
-      baseDir: 'app',
-      startPath: 'main.html'
-    }
+      baseDir: 'dist'
+    },
+    port: 8080,
+    startPath: 'main.html'
   });
 });
 
-//método que observa quais arquivos são modificados e quando rodar um tarefa
-gulp.task('watch', ['browserSync', 'sass'], function(){
+//método que observa quais arquivos são modificados e quando rodar uma tarefa
+gulp.task('watch', function(){
   //gulp.watch('files-to-watch', ['tasks', 'to', 'run']); //exemplo
-  // um watch separado pra cada tipo de arquivo
-  gulp.watch('app/scss/**/*.+(scss|sass)', ['sass']);
-  gulp.watch('app/js/**/*.js', browserSync.reload);
-  gulp.watch('app/templates/**/*.html', browserSync.reload);
-});
-
-//definir sequência de tarefas (exemplo)
-gulp.task('build-example', function(callback){
-  runSequence('task-one', ['tasks', 'two', 'run', 'in', 'parallel'], 'task-three', callback);
+  //um watch separado pra cada tipo de arquivo
+  gulp.watch('src/sass/**/*.+(scss|sass)', ['sass']);
+  gulp.watch('src/js/**/*.js', ['uglify']);
+  gulp.watch('src/templates/**/*.hbs', ['handlebars']);
+  gulp.watch('src/img/**/*.+(png|jpg|jpeg|gif|svg)', ['images']);
+  gulp.watch('src/fonts/*.+(eot|ttf|woff|woff2|svg)', ['fonts']);
 });
 
 //compilar projeto final
 gulp.task('build', function(callback){
   runSequence(
     'clean:dist',
-    'sass',
-    ['useref', 'fonts'],
+    ['sass', 'uglify', 'handlebars', 'fonts', 'images'],
     callback
   );
 });
@@ -117,7 +140,7 @@ gulp.task('build', function(callback){
 //tarefa padrão
 gulp.task('default', function(callback){
   runSequence(
-    'sass',
+    'build',
     ['browserSync', 'watch'],
     callback
   );
